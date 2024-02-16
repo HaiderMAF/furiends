22f575ff28a090d2a10438b457aef352dff0e0b6
haiderfarooqui <haider.farooqui@georgebrown.ca>
2024-02-16 07:04:42 -0500

    first commit

---
+++ src/components/Nav.js
@@ -0,0 +1,27 @@
+import logo from '../images/logo-no-background.png'
+import logoColor from '../images/logo-color.png'
+
+const Nav = ({ minimal, authToken, setShowModal, showModal, setIsSignUp}) => {
+
+    const handleClick = () => {
+        setShowModal(true)
+        setIsSignUp(false)
+    }
+
+    return (
+        <nav>
+            <div className="logo-container">
+                <img className="logo-container" src={minimal ? logoColor : logo} />
+            </div>
+            {/*Show Login If only not authenticated and not logged in in AND Disable Button When
+            Modal is Shown */}
+            {!authToken && !minimal && <button
+                className="nav-button"
+                onClick={handleClick}
+                disabled= {showModal}
+            >Login</button>}
+        </nav>
+    )
+}
+
+export default Nav;
